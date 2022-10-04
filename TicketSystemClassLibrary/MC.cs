c86918be53e5d2893a8c13fe74b93ba622a56382
seb99seb <sebvangpedersen@hotmail.dk>
2022-10-04 10:59:22 +0200

    Task 3 done

---
+++ TicketSystemClassLibrary/MC.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketSystemClassLibrary
+{
+    /// <summary>
+    /// Public class MC
+    /// Contains properties string LicensePlate and DateTime Date,
+    /// and methods Price() and VehicleType().
+    /// </summary>
+    public class MC
+    {
+        public string LicensePlate { get; set; }
+        public DateTime Date { get; set; }
+        /// <summary>
+        /// Method to return double - set to 125
+        /// </summary>
+        /// <returns></returns>
+        public double Price()
+        {
+            return 125;
+        }
+        /// <summary>
+        /// Method to return string "MC"
+        /// </summary>
+        /// <returns></returns>
+        public string VehicleType()
+        {
+            return "MC";
+        }
+    }
+}
---
+++ TicketSystemClassLibraryTests/MCTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TicketSystemClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketSystemClassLibrary.Tests
+{
+    [TestClass()]
+    public class MCTests
+    {
+        [TestMethod()]
+        public void PriceTest()
+        {
+            MC mc = new MC();
+            double ActualValue = mc.Price();
+            double ExpectedValue = 125;
+            Assert.AreEqual(ExpectedValue, ActualValue);
+        }
+        [TestMethod()]
+        public void VehicleTypeTestSucess()
+        {
+            MC mc = new MC();
+            string ActualValue = mc.VehicleType();
+            string ExpectedValue = "MC";
+            Assert.AreEqual(ExpectedValue, ActualValue);
+        }
+    }
+}
