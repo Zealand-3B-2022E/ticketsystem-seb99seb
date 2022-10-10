4549c55e2232c822516b4b9ecd0f0153d299f5fd
seb99seb <sebvangpedersen@hotmail.dk>
2022-10-10 14:50:58 +0200

    task 9 done

---
+++ StoreBaeltTicketLibrary/SBCar.cs
@@ -0,0 +1,34 @@
+﻿using TicketSystemClassLibrary;
+
+namespace StoreBaeltTicketLibrary
+{
+    public class SBCar : Car
+    {
+        /// <summary>
+        /// New bool property, used to detertime whether or not to give 20% discount
+        /// </summary>
+        public bool Weekend { get; set; }
+        public SBCar(string licenseplate):base(licenseplate)
+        {
+
+        }
+        /// <summary>
+        /// Price method that returns double 240 by default, can however get discount by both or Weekend discount and Brobizz
+        /// </summary>
+        /// <returns></returns>
+        public override double Price()
+        {
+            double tempvar = 240;
+            if (Weekend)
+            {
+                tempvar -= (tempvar * 0.2);
+            }
+            if (Brobizz)
+            {
+                tempvar -= (tempvar * 0.05);
+            }
+            return tempvar;
+        }
+
+    }
+}
---
+++ StoreBaeltTicketLibraryTests/SBCarTests.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StoreBaeltTicketLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicketSystemClassLibrary;
+
+namespace StoreBaeltTicketLibrary.Tests
+{
+    [TestClass()]
+    public class SBCarTests
+    {
+        [TestMethod()]
+        public void PriceTestNoDiscount()
+        {
+            double ExpectedValue = 240;
+            SBCar car = new SBCar("A7BH3AS");
+            double ActualValue = car.Price();
+            Assert.AreEqual(ExpectedValue, ActualValue);
+        }
+        [TestMethod()]
+        public void PriceTestWithWeekendAndBrobizz()
+        {
+            double ExpectedValue = 182.4;
+            SBCar car = new SBCar("A7BH3AS");
+            car.Brobizz = true;
+            car.Weekend = true;
+            double ActualValue = car.Price();
+            Assert.AreEqual(ExpectedValue, ActualValue);
+        }
+        [TestMethod()]
+        public void PriceTestWithWeekend()
+        {
+            double ExpectedValue = 192;
+            SBCar car = new SBCar("A7BH3AS");
+            car.Weekend = true;
+            double ActualValue = car.Price();
+            Assert.AreEqual(ExpectedValue, ActualValue);
+        }
+        [TestMethod()]
+        public void PriceTestWithBrobizz()
+        {
+            double ExpectedValue = 228;
+            SBCar car = new SBCar("A7BH3AS");
+            car.Brobizz = true;
+            double ActualValue = car.Price();
+            Assert.AreEqual(ExpectedValue, ActualValue);
+        }
+    }
+}
