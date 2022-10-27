855b0f46d56ee61aae01fdc2d78c2e19114cbac0
Marcus Vibe Hjulgaard <marc3277@edu.easj.dk>
2022-10-27 13:21:22 +0200

    oprettet car object ude for unit testene, så man ikke behøver at lave et object for hver unit test

--- TicketSystemClassLibraryTests/CarTests.cs
+++ TicketSystemClassLibraryTests/CarTests.cs
@@ -11,19 +11,20 @@ namespace TicketSystemClassLibrary.Tests
     [TestClass()]
     public class CarTests
     {
+        // så kan du bruge objectet car på alle dine unit test i stedenfor at skrive en ny for hver unitest
+        Car car = new("A7BH3AS");
+
         [TestMethod()]
         public void PriceTest()
         {
-            double ExpectedValue = 240;
-            Car car = new Car("A7BH3AS");
+            double ExpectedValue = 240;           
             double ActualValue = car.Price();
             Assert.AreEqual(ExpectedValue,ActualValue);
         }
         [TestMethod()]
         public void PriceTestDiscount()
         {
-            double ExpectedValue = 228;
-            Car car = new Car("A7BH3AS");
+            double ExpectedValue = 228;            
             car.Brobizz = true;
             double ActualValue = car.Price();
             Assert.AreEqual(ExpectedValue, ActualValue);
@@ -32,16 +33,14 @@ namespace TicketSystemClassLibrary.Tests
         [TestMethod()]
         public void VehicleTypeTestSucess()
         {
-            string ExpectedValue = "Car";
-            Car car = new Car("A7BH3AS");
+            string ExpectedValue = "Car";            
             string ActualValue = car.VehicleType();
             Assert.AreEqual(ExpectedValue, ActualValue);
         }
         [TestMethod()]
         public void LicensePlateTestSuccess()
         {
-            string ExpectedValue = "A7BH3AS";
-            Car car = new Car("A7BH3AS");
+            string ExpectedValue = "A7BH3AS";            
             string ActualValue = car.LicensePlate;
             Assert.AreEqual(ExpectedValue, ActualValue);
         }
